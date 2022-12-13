72423e75f5a2d60e8625c2b4d7c6c9644f27478e
DiegoHuacanjulca <diant2405@gmail.com>
2022-12-13 12:52:11 -0500

    add logic tournament init, get winner player and history oturnament

--- app/Http/Controllers/TournamentController.php
+++ app/Http/Controllers/TournamentController.php
@@ -13,17 +13,11 @@ class TournamentController extends Controller
 {
     public function store(StoreTournamentRequest $request) {
 
-        /* {
-            "name": "Mundial de galletitas",
-            "player_ids": [2,1],
-            "gender_id": 2
-        }*/
-
         $tournament = Tournament::create($request->only('name', 'gender_id'));
 
         // store tournament_players
-        $player_ids = $request->input('player_ids');
-        $players = Player::whereIn('id', $player_ids)->get(['id', 'name', 'gender_id']);
+        $playerIds = $request->input('player_ids');
+        $players = Player::whereIn('id', $playerIds)->get(['id', 'name', 'gender_id']);
         
         foreach($players as $player) {
             TournamentPlayer::create([
@@ -35,5 +29,48 @@ class TournamentController extends Controller
             ]);
         }
         
+        shuffle($playerIds); // desordenar array ids    
+        
+        // logica de torneo
+        $tournament_history = [];
+        $winner = $this->tournamentinit($playerIds, $tournament_history);
+        
+        dd($winner);
+    }
+
+    public function tournamentinit($playerIds, $tournament_history) {
+        
+        $n = count($playerIds);   
+        
+        if ($n == 1) { dump($tournament_history);
+            return $playerIds;
+        }
+
+        $array_encuentros = [];
+        $array_winnings = [];
+        
+        for($i = 0; $i <= ($n/2) - 1 ; $i++) {    
+            
+            // Establece un player ganador con base en sus skills
+            $winningPlayer = $this->matchPlayer($playerIds[$i], $playerIds[$n-$i-1]);
+
+            // Actualiza arreglo con ganadores de la primera ronda
+            array_push($array_winnings, $winningPlayer);    
+            
+            // Guarda los encuentros con sus respectivos ganadores para tener un historial del torneo
+            array_push($array_encuentros, [
+                'players' => [ $playerIds[$i], $playerIds[$n-$i-1] ],
+                'winner' => $winningPlayer
+            ]);
+        }
+
+        // Guarda historial de todas las fases
+        array_push($tournament_history, $array_encuentros);
+
+        $this->tournamentinit($array_winnings, $tournament_history);
+    }
+
+    public function matchPlayer($playerA, $playerB) {
+        return $playerB; // lógica provisional, da como ganador siempre al segundo jugador
     }
 }
